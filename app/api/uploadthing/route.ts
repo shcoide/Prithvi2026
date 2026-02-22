c789fde50480d3164941009cb9a556c55137ca74
Shivam <shivamshibu2003@gmail.com>
2026-02-22 11:15:25 +0530

    add:uploadthing7

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,6 +1,11 @@
+
 import { createRouteHandler } from "uploadthing/next";
-import { ourFileRouter } from "./core";
+import { ourFileRouter } from "./core"; // Or wherever your core.ts is
 
 export const { GET, POST } = createRouteHandler({
-    router: ourFileRouter,
+  router: ourFileRouter,
+  config: {
+    // This forces Uploadthing to explicitly ping your live domain!
+    callbackUrl: "https://prithvi2026.com/api/uploadthing",
+  },
 });
